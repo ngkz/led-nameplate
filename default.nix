ec75622f2c6ee70a5f46578bbde8d16edbbde52f
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2024-02-28 12:15:19 +0900

    nix env

---
+++ default.nix
@@ -0,0 +1,7 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [
+      (python3.withPackages (p: with p; [ pyusb pillow ]))
+    ];
+}
